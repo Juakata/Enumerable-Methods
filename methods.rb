f8b3dca76277d4c625e8ca3e7b882df19500819b
Juakata <ha.uzquiano@hotmail.com>
2019-08-27 16:40:34 -0600

    all method is done

--- methods.rb
+++ methods.rb
@@ -14,8 +14,40 @@ module Enumerable
       i+=1
     end
   end
+
+  def my_select
+    arr = []
+    i = 0
+    while i < self.length
+      arr.push(self[i]) if yield(self[i])
+      i+=1
+    end
+    arr
+  end
+
+  def my_all
+    result = true
+    i = 0
+    while i < self.length
+      if !yield(self[i])
+        result=false
+      end
+      i+=1
+    end
+    result
+  end
+end
+
+#testing the methods
+
+[1,2,3,4,5].my_each do |i|
+  puts i
 end
 
 %w[cat dog horse].my_each_with_index do |value,i|
   puts "#{value} is at position #{i}"
 end
+
+print [1,2,3,4,5,6].my_select { |n| n.odd? }
+
+print [1,2,3,4,5,6].my_all { |n| n>0 }
