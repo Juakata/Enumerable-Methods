d49450d88a5801b8a199fb717cf22ccea3144cd3
Juakata <ha.uzquiano@hotmail.com>
2019-09-02 11:52:30 -0600

    adding files for spec and first test is done

---
+++ spec/method_spec.rb
@@ -0,0 +1,12 @@
+#spec/calculator_spec.rb
+
+require './methods.rb'
+
+RSpec.describe Enumerable do
+  let(:test_array_1) { [1,2,3,4,5] }
+  describe "#my_each" do
+    it "returns all the elements in the array" do
+      expect(test_array_1.my_each { |e| e }).to eql(test_array_1)
+    end
+  end
+end
