90145d785f4da2d3b88d71f2fa57f021a5ce0949
Juakata <ha.uzquiano@hotmail.com>
2019-09-02 13:58:48 -0600

    changing block

--- spec/method_spec.rb
+++ spec/method_spec.rb
@@ -6,7 +6,7 @@ require './methods.rb'
 
 RSpec.describe Enumerable do
   let(:test_array_1) { [1, 2, 3, 4, 5] }
-  let(:test_array_2) { [1, 3, 5] }
+  let(:test_array_2) { [3] }
   let(:test_array_3) { [2, 4, 6, 8, 10] }
   describe '#my_each' do
     it 'returns all elements in the array' do
@@ -22,43 +22,43 @@ RSpec.describe Enumerable do
 
   describe '#my_select' do
     it 'returns an array depending on the condition' do
-      expect(test_array_1.my_select { |e| e%2 != 0 }).to eql(test_array_2)
+      expect(test_array_1.my_select { |e| e % 3 == 0 }).to eql(test_array_2)
     end
   end
 
   describe '#my_all' do
     it 'returns true if all the elements meet the condition' do
-        expect(test_array_1.my_all? { |e| e%2 != 0 }).to eql(false)
+      expect(test_array_1.my_all? { |e| e % 3 == 0 }).to eql(false)
     end
   end
 
   describe '#my_any' do
     it 'returns true if any elements meet the condition' do
-        expect(test_array_1.my_any? { |e| e%2 != 0 }).to eql(true)
+      expect(test_array_1.my_any? { |e| e % 3 == 0 }).to eql(true)
     end
   end
 
   describe '#my_none' do
     it 'returns true if none elements meet the condition' do
-        expect(test_array_1.none? { |e| e%2 != 0 }).to eql(false)
+      expect(test_array_1.none? { |e| e % 3 == 0 }).to eql(false)
     end
   end
 
   describe '#my_count' do
     it 'returns how many times is the number on the array' do
-        expect(test_array_1.my_count(3)).to eql(1)
+      expect(test_array_1.my_count(3)).to eql(1)
     end
   end
 
   describe '#my_map' do
     it 'returns a new array that meets the condition' do
-        expect(test_array_1.my_map { |e| e*2 }).to eql(test_array_3)
+      expect(test_array_1.my_map { |e| e*2 }).to eql(test_array_3)
     end
   end
 
   describe '#my_inject' do
     it 'returns a new array that meets the condition' do
-        expect(test_array_1.my_inject(5) { |sum,e| sum + e} ).to eql(20)
+      expect(test_array_1.my_inject(5) { |sum,e| sum + e} ).to eql(20)
     end
   end
 end
