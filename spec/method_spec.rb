186f1d144eca8182899c645d3a7a52e5e7409765
Juakata <ha.uzquiano@hotmail.com>
2019-09-02 12:03:45 -0600

    fixing issues with helper file

--- spec/method_spec.rb
+++ spec/method_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # spec/calculator_spec.rb
 
 require './methods.rb'
@@ -7,7 +8,7 @@ RSpec.describe Enumerable do
   let(:test_array_1) { [1, 2, 3, 4, 5] }
   describe '#my_each' do
     it 'returns all the elements in the array' do
-      expect(test_array_1.my_each { |e| e } ).to eql(test_array_1)
+      expect(test_array_1.my_each { |e| e }).to eql(test_array_1)
     end
   end
 end
