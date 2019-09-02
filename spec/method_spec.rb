b802a847f7ee415fc839457b69832ab08beef87f
Juakata <ha.uzquiano@hotmail.com>
2019-09-02 12:00:50 -0600

    fixing issues with stickler

--- spec/method_spec.rb
+++ spec/method_spec.rb
@@ -1,12 +1,13 @@
-#spec/calculator_spec.rb
+# frozen_string_literal: true
+# spec/calculator_spec.rb
 
 require './methods.rb'
 
 RSpec.describe Enumerable do
-  let(:test_array_1) { [1,2,3,4,5] }
-  describe "#my_each" do
-    it "returns all the elements in the array" do
-      expect(test_array_1.my_each { |e| e }).to eql(test_array_1)
+  let(:test_array_1) { [1, 2, 3, 4, 5] }
+  describe '#my_each' do
+    it 'returns all the elements in the array' do
+      expect(test_array_1.my_each { |e| e } ).to eql(test_array_1)
     end
   end
 end
